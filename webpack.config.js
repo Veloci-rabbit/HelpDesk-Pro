b3ab7d86f959848611c39a1abb0da06f4aa8d457
Caner Demir <canerdemir62@gmail.com>
2021-01-02 20:37:42 -0800

    create .env file to hide database connection strings

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   entry: './src/index.js',
@@ -50,6 +51,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: 'src/index.html',
     }),
+    new Dotenv
   ],
   resolve: {
     extensions: ['.js', '.jsx'],
